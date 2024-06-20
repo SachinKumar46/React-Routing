7938c853f941b5161f7ec37fbbb2558dd210dad9
Sachin Rajput <sachinrajput17798@gmail.com>
2024-06-20 18:04:13 +0530

    add some files and we can do routing

---
+++ src/components/Service.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Service() {
+  return (
+    <div>
+        <div className="container">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis eligendi necessitatibus perspiciatis, explicabo laborum beatae voluptate facilis numquam tempora enim doloremque rerum ipsam. Ea, ipsam nisi ratione eligendi necessitatibus placeat.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis eligendi necessitatibus perspiciatis, explicabo laborum beatae voluptate facilis numquam tempora enim doloremque rerum ipsam. Ea, ipsam nisi ratione eligendi necessitatibus placeat.
+        </div>
+    </div>
+  )
+}
